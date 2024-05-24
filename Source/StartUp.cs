8d8869c29ad971ceac2736714b73f5c6132a6627
MaxDorob <maxdor81@gmail.com>
2024-05-25 04:04:09 +0600

    Temp patch

---
+++ Source/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shashlichnik
+{
+    [Verse.StaticConstructorOnStartup()]
+    public static class StartUp
+    {
+        static StartUp()
+        {
+            new Harmony("Shashlichnik.AnimatedPawnsFramework").PatchAll();
+        }
+    }
+}
