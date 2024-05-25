8ac1bdc2d90f8ab4de7e5275a0a604287fc52e9a
MaxDorob <maxdor81@gmail.com>
2024-05-26 01:03:30 +0600

    Changed rimworld back layer cap from 10 to 50

---
+++ Source/Layers_Patch.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace Shashlichnik
+{
+    [HarmonyPatch(typeof(PawnRenderUtility), nameof(PawnRenderUtility.AltitudeForLayer))]
+    internal static class Layers_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            foreach (var instruction in instructions)
+            {
+                if (instruction.operand is float && (float)instruction.operand < 0f)
+                {
+                    instruction.operand = -50f;
+                }
+                yield return instruction;
+            }
+        }
+    }
+}
