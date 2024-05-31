53c03b86040a01035ce2cf7a833661b3628e24d7
MaxDorob <maxdor81@gmail.com>
2024-06-01 03:38:30 +0600

    Removed harmony recache patch, changed Graphic Get process

--- Source/PawnRenderNodeWorker_Animated.cs
+++ Source/PawnRenderNodeWorker_Animated.cs
@@ -10,27 +10,13 @@ namespace Shashlichnik
 {
     public class PawnRenderNodeWorker_Animated : PawnRenderNodeWorker
     {
-        protected override Material GetMaterial(PawnRenderNode node, PawnDrawParms parms)
+        protected override Graphic GetGraphic(PawnRenderNode node, PawnDrawParms parms)
         {
             if (node is PawnRenderNode_Animated animated)
             {
-                Graphic graphic = animated.Graphic;
-                if (graphic == null)
-                {
-                    return null;
-                }
-                if (node.Props.flipGraphic && parms.facing.IsHorizontal)
-                {
-                    parms.facing = parms.facing.Opposite;
-                }
-                Material material = graphic.NodeGetMat(parms);
-                if (material != null && !parms.Portrait && parms.flags.FlagSet(PawnRenderFlags.Invisible))
-                {
-                    material = InvisibilityMatPool.GetInvisibleMat(material);
-                }
-                return material;
+                return animated.Graphic;
             }
-            return base.GetMaterial(node, parms);
+            return base.GetGraphic(node, parms);
         }
     }
 }
