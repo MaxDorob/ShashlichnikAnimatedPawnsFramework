ae76cb8cd3434ebdcac9e0b894649464fa25d1d4
MaxDorob <maxdor81@gmail.com>
2024-06-01 03:40:46 +0600

    Changed PawnRenderNode

--- Source/PawnRenderNode_Animated.cs
+++ Source/PawnRenderNode_Animated.cs
@@ -63,30 +63,24 @@ namespace Shashlichnik
         }
 
         //public KeyframeExtended NextKeyframe => Props.keyframes.FirstOrDefault() ?? Props.keyframes.First();
-        public new bool RecacheRequested
+
+
+
+        protected override IEnumerable<Graphic> GraphicsFor(Pawn pawn)
         {
-            get
+            if (graphics == null && HasGraphic(pawn))
             {
-                if (pawnDead)
+                graphics = new Dictionary<KeyframeExtended, Graphic>(Props.keyframes.Count);
+                for (int i = 0; i < Props.keyframes.Count; i++)
                 {
-                    return false;
+                    var keyframe = Props.keyframes[i];
+                    var graphic = GraphicFor(pawn, string.IsNullOrWhiteSpace(keyframe.texPath) ? TexPathFor(pawn) + i.ToString() : keyframe.texPath);
+                    graphics.Add(keyframe, graphic);
+                    yield return graphic;
                 }
-                if (Math.Abs(tickManager.TicksAbs - lastRecachedAbsTick) > 30)
-                {
-                    currentKeyframe = null;
-                    return true;
-                }
-                var currentKf = CurrentKeyframe;
-                var currentTick = CurrentAnimationTick;
-                if (Props.keyframes[Props.keyframes.Count - 1] == currentKeyframe)
-                {
-                    return currentTick <= Props.keyframes[Props.keyframes.Count - 2].tick;
-                }
-                return currentKf.tick >= currentTick;
             }
         }
-
-        public new Graphic Graphic => graphics[CurrentKeyframe];//Change it to override when next update releases
+        public new Graphic Graphic => graphics[CurrentKeyframe];
         public virtual Graphic GraphicFor(Pawn pawn, string text)
         {
             if (text.NullOrEmpty())
@@ -99,20 +93,6 @@ namespace Shashlichnik
                 return null;
             }
             return GraphicDatabase.Get<Graphic_Multi>(text, shader, Vector2.one, ColorFor(pawn));
-        }
-        protected override void EnsureMaterialsInitialized()
-        {
-            base.EnsureMaterialsInitialized();
-            var pawn = tree.pawn;
-            if (graphics == null && HasGraphic(pawn))
-            {
-                graphics = new Dictionary<KeyframeExtended, Graphic>(Props.keyframes.Count);
-                for (int i = 0; i < Props.keyframes.Count; i++)
-                {
-                    var keyframe = Props.keyframes[i];
-                    graphics.Add(keyframe, GraphicFor(pawn, string.IsNullOrWhiteSpace(keyframe.texPath) ? TexPathFor(pawn) + i.ToString() : keyframe.texPath));
-                }
-            }
         }
 
         Dictionary<KeyframeExtended, Graphic> graphics;
