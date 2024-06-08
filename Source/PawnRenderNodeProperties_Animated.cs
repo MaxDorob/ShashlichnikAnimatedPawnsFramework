2d90ddb23ac2ed1d2bda7e9a274b0fc8e07ee99b
MaxDorob <maxdor81@gmail.com>
2024-06-08 21:08:30 +0600

    Keyframes can be specified by new properties

--- Source/PawnRenderNodeProperties_Animated.cs
+++ Source/PawnRenderNodeProperties_Animated.cs
@@ -15,11 +15,34 @@ namespace Shashlichnik
             nodeClass = typeof(PawnRenderNode_Animated);
         }
         public int tickOffset = 0;
+        public int keyframesCount = 0;
+        public int ticksPerAnimation = 3;
+        public string keyframesTexPath;
         public List<KeyframeExtended> keyframes;
         public class KeyframeExtended : Keyframe
         {
             public string texPath;
         }
+        public override void ResolveReferences()
+        {
+            base.ResolveReferences();
+            if (keyframes == null)
+            {
+                keyframes = new List<KeyframeExtended>();
+            }
+            if (keyframes.Count == 0)
+            {
+                if (keyframesCount <= 0 || keyframesTexPath.NullOrEmpty())
+                {
+                    Log.Error($"Keyframes not specified");
+                }
+                for (int i = 0; i < keyframesCount; i++)
+                {
+                    keyframes.Add(new KeyframeExtended() { texPath = keyframesTexPath + (i + 1), tick = i * ticksPerAnimation });
+                }
+            }
+        }
+
     }
 
 }
