a739f91a5f481fef0bd94a01ad1e5e4e890e7832
MaxDorob <maxdor81@gmail.com>
2024-06-30 06:21:02 +0600

    Rotation utility

---
+++ Source/PawnRenderSubWorker_RotateByRot.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace Shashlichnik
+{
+    public class PawnRenderSubWorker_RotateByRot : PawnRenderSubWorker
+    {
+        public override void TransformRotation(PawnRenderNode node, PawnDrawParms parms, ref Quaternion rotation)
+        {
+            rotation = rotation * Quaternion.AngleAxis(parms.facing.AsAngle, Vector3.up);
+        }
+    }
+}
